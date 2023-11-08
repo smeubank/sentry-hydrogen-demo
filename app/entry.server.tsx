c7743b46062a0d42e73553c10136f26c954eb342
Steven Eubank <eubank.steven88@gmail.com>
2023-11-08 23:06:16 +0100

    test yank of CSP

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -23,44 +23,4 @@ export async function handleError(
   {request}: DataFunctionArgs,
 ): Promise<void> {
   SentryRemix.captureRemixServerException(error, 'remix.server', request);
-}
-
-export default async function handleRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext,
-) {
-  const {nonce, header, NonceProvider} = createContentSecurityPolicy();
-
-  const body = await renderToReadableStream(
-    <NonceProvider>
-      <RemixServer context={remixContext} url={request.url} />
-    </NonceProvider>,
-    {
-      nonce,
-      signal: request.signal,
-      onError(error) {
-        // eslint-disable-next-line no-console
-        console.error(error);
-        responseStatusCode = 500;
-      },
-    },
-  );
-
-  if (isbot(request.headers.get('user-agent'))) {
-    await body.allReady;
-  }
-
-  responseHeaders.set('Content-Type', 'text/html');
-  responseHeaders.set('Content-Security-Policy', header);
-
-  // Add the document policy header to enable JS profiling
-  // This is required for Sentry's profiling integration
-  responseHeaders.set('Document-Policy', 'js-profiling');
-
-  return new Response(body, {
-    headers: responseHeaders,
-    status: responseStatusCode,
-  });
-}
+};
